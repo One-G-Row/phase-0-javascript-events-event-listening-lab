02fd322f63620e955ce48d5b3e79be8730479a16
student <kmac.kimaru93@gmail.com>
2024-04-01 12:34:13 +0300

    adding eventsevent listener

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
+const input = document.getElementById("button");
+
 function addingEventListener() {
+  input.addEventListener("click", addingEventListener);
+  alert("I was clicked!");
 }
+addingEventListener();
